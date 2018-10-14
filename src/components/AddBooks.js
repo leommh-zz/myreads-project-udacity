021f1e1a9f93f825de0212ddabca16ad5d4412a6
Leonardo Morini <leo.morini.nn@gmail.com>
2018-10-14 16:09:19 -0300

    first commit

---
+++ src/components/AddBooks.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Search from './Search';
+
+class AddBooks extends Component{
+
+    render(){
+
+        return(
+            <div>
+                <Search />
+                <h2>AddBooks</h2>
+            </div>
+        );
+    }
+}
+
+export default AddBooks;
